3a71d54f66cf53f9c246ab9a1c9de6139c133d12
Brian Nuebling <brian@electriccitizen.com>
2022-09-08 15:32:57 -0500

    Drupal coder fixes for citizen_preview module

--- web/modules/custom/citizen_preview/js/citizen_preview.js
+++ web/modules/custom/citizen_preview/js/citizen_preview.js
@@ -1,16 +1,16 @@
-(function($, Drupal) {
+(function ($, Drupal) {
 
 /* PREVIEW ACTION & FIXED
 ----------------------- */
 Drupal.behaviors.previewAction = {
-	attach: function (context, settings) {
-    $('#edit-citizen-preview--2', context).once('has-preview').each(function(){
-			//trigger preview
-		  $(this).on('click',function(e) {
-		    e.preventDefault();
-		    $('.responsive-preview-icon-active').trigger('click');
-		  });
-		});
+    attach: function (context, settings) {
+    $('#edit-citizen-preview--2', context).once('has-preview').each(function () {
+            //trigger preview
+          $(this).on('click',function (e) {
+            e.preventDefault();
+            $('.responsive-preview-icon-active').trigger('click');
+          });
+        });
   }
 };
 
