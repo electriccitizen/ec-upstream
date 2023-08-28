22d14b0a1f3fecb98ddadba0c9c727edac05056f
David Loop <davidloop@gmail.com>
2023-08-28 11:45:04 -0500

    ECUP-215, ECUP-218
    215: Move site search to a views block placed using layout builder
    218: Switch to core drupal.debounce

--- web/themes/custom/citizen_dart/components/03-composites/blocks/search/search-block.js
+++ web/themes/custom/citizen_dart/components/03-composites/blocks/search/search-block.js
@@ -1,27 +1,40 @@
+
 (function($, Drupal, once) {
 	
-//search toggle
-Drupal.behaviors.searchToggle = {
-	attach: function (context, settings) {
-	 	$(once('tSearch', '.block-site-search', context)).each(function(){
-	 		$('.t-search', this).click(function(e){
-	 			e.preventDefault();
-	 			$('#search-form-wrapper').slideDown(400).attr('aria-hidden', 'false');
-	 			$('.t-search-close').delay(300).fadeIn(300);
-	 			//close main menu if open when search is clicked
-	 			$('#superfish-main-accordion.sf-expanded').slideUp(200);
-	 			setTimeout(function(){
-		 			$('#superfish-main-toggle,#superfish-main-accordion').removeClass('sf-expanded');
-		 		}, 200);
-	 		});
-	 		$('.close-search', this).click(function(e){
-	 			e.preventDefault();
-	 			$('.t-search-close').fadeOut(300);
-	 			$('#search-form-wrapper').delay(200).slideUp(400).attr('aria-hidden', 'true');
-	 		});	
-		});
-	}
-}
+  // search toggle
+  Drupal.behaviors.searchToggle = {
+    attach: function (context, settings) {
+      $(once('tSearch', '.block-site-search', context)).each( () => {
+
+        const openTrigger = $(".t-search"),
+              closeTrigger = $(".close-search"),
+              target = $(".t-search-close");
+
+        // open search
+        const openSearchBlock = (e) => {
+          e.preventDefault();
+          $("#search-form-wrapper").slideDown('fast').attr('aria-hidden', 'false');
+          $(target).delay(300).fadeIn('fast');
+
+          // close main menu if open when search is clicked
+          $("#superfish-main-accordion.sf-expanded").slideUp(200);
+          setTimeout( () => {
+            $("#superfish-main-toggle, #superfish-main-accordion").removeClass('sf-expanded');
+          }, 200);
+        }
+        $(openTrigger).on('click', openSearchBlock);
+
+        // close search
+        const closeSearchBlock = (e) => {
+          e.preventDefault();
+          $(target).fadeOut('fast');
+          $("#search-form-wrapper").delay(200).slideUp('fast').attr('aria-hidden', 'true');
+        }
+        $(closeTrigger).on('click', closeSearchBlock);
+
+      });
+    }
+  }
 
 })(jQuery, Drupal, once);
 
