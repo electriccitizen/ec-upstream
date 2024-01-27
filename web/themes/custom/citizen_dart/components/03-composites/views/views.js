5da09ab8716851742ae21331bbe42493c0983a51
Adam Fuchs <adam@electriccitizen.com>
2024-01-27 13:57:21 -0600

    Fix site search, remove unneeded custom search block type

--- web/themes/custom/citizen_dart/components/03-composites/views/views.js
+++ web/themes/custom/citizen_dart/components/03-composites/views/views.js
@@ -4,14 +4,14 @@
 ------------------------------------ */
 Drupal.behaviors.viewsScroll = {
   attach: function (context, settings) {
-    $(once('wasSearched', '.views-exposed-form', context)).each(function(){
+    $(once('wasSearched', '#view-scroll-anchor', context)).each(function(){
       //check if searched
       var urlCurrent = window.location.href;
       if((urlCurrent.indexOf("?f") > -1) || (urlCurrent.indexOf("?c") > -1) || (urlCurrent.indexOf("?s") > -1)){
 				$(document).ready(function(){
 					setTimeout(function() {
 			   			$('html, body').animate({
-							scrollTop: $('.view .exposed-form-container').offset().top - 10
+							scrollTop: $('#view-scroll-anchor').offset().top - 10
 			   			});
 		   			}, 300);
 		 		});
@@ -19,5 +19,32 @@ Drupal.behaviors.viewsScroll = {
     });
   }
 };
+
+//highlight search results
+Drupal.behaviors.searchHighlight = {
+	attach: function (context, settings) {
+	 	$(once('tSearch', '#views-exposed-form-site-search-site-search-block', context)).each(function(){
+      //find searched value if not null and highlight each word
+      if($('#edit-site-search-api-fulltext', this).val()){
+        var searchString = $('#edit-site-search-api-fulltext', this).val().split(' ');
+        $.fn.wrapInTag = function(opts) {
+
+          var tag = opts.tag
+            , words = opts.words || []
+            , regex = RegExp(words.join('|'), 'gi') // case insensitive
+            , replacement = '<'+ tag +'>$&</'+ tag +'>';
+
+          return this.html(function() {
+            return $(this).text().replace(regex, replacement);
+          });
+        };
+        $('.views-field-body').wrapInTag({
+          tag: 'mark class="search-highlight"',
+          words: searchString
+        });
+      }
+		});
+	}
+}
 
 })(jQuery, Drupal, once);
