f8c5f711089fd5b4a4c26ba0fcb20ce4722373ce
Adam Fuchs <adam@electriccitizen.com>
2024-02-03 09:25:12 -0600

    Remove toolbar poisition junk

--- web/themes/custom/citizen_dart/components/05-assembly/global/site.js
+++ web/themes/custom/citizen_dart/components/05-assembly/global/site.js
@@ -36,42 +36,4 @@ Drupal.behaviors.backToTop = {
   }
 }
 
-/* TOOLBAR POSITION
------------------- */
-Drupal.behaviors.toolbarPosition = {
-  attach: function (context, settings) {
-    $(once('toolbar-position', '[id=block-toolbarposition]', context)).each( () => {
-      
-      const toolbar = $("[id=block-toolbarposition]");
-      const layout = $(".main-page-content");
-      const overflow = $(".overflow-guard");
-      const key = $(toolbar).find(".field-toolbar-position").html();
-      const tasks = ("[id=block-citizen-dart-local-tasks]");
-      const message = $(".drupal-message");
-
-      function runToolbar() {
-        if ( $(toolbar).length ) {
-          if ( key != 1 ) {
-            $(layout).addClass('toolbar-bottom');
-            $(overflow).addClass('toolbar-bottom-push');
-
-            if ( $(message).length ) {
-              $(overflow).css({
-                'padding-bottom' : $(tasks).outerHeight() + $(message).outerHeight() + 'px',
-              });
-            } else {
-              $(overflow).css({
-                'padding-bottom' : $(tasks).outerHeight() + 'px',
-              });
-            }
-          }
-        }
-      }
-
-      $(window).on('load', runToolbar);
-
-    });
-  }
-}
-
 })(jQuery, Drupal, once);
