8c413893975c52f66a6319bb53276dc55735f708
Adam Fuchs <adam@electriccitizen.com>
2024-02-28 13:12:19 -0600

    Header theming

--- web/themes/custom/mstate/components/05-assembly/global/site.js
+++ web/themes/custom/mstate/components/05-assembly/global/site.js
@@ -1,4 +1,41 @@
-(function(Drupal, once) {
+(function($, Drupal, once) {
+
+/* add secondary and search to mobile menu
+--------------------- */
+Drupal.behaviors.mobileMenu = {
+  attach: function (context, settings) {
+    $(once('insertHeaderElements', '.block-superfishmain', context)).each(function() {
+      $(document).ready(function() {
+        $(window).on('resize', debounce(mobileMenuInsert, 10)).trigger('resize');
+      });
+    });
+  }
+};
+
+//function to move blocks to mobile menu
+function mobileMenuInsert() {
+	var wwidth = $(window).outerWidth();
+	if (wwidth < 1161) {
+		//if not in desktop or higher, clone the search and secondary menu and move them to the mobile menu
+		if(!$('.sf-accordion > li.mobile-search-container').length){
+			const $searchContainer = $('.site-header #site-search-form');
+      
+			// Continue with cloning and moving
+			$searchContainer.clone(true)
+				.prependTo('.sf-accordion')
+				.wrap('<li class="mobile-search-container"></li>')
+				.addClass('mobile-search');
+
+      const $quickLinksMenu = $('.site-header #block-quicklinks-menu > .quicklinks-drawer > ul');
+      // Continue with cloning and moving
+			$quickLinksMenu.clone(true)
+				.appendTo('.sf-accordion')
+				.wrap('<li class="mobile-quicklinks-container"></li>')
+				.addClass('mobile-quicklinks');
+		}
+	}
+}//end mobile menu function
+
 
 
 /* BACK TO TOP
@@ -25,4 +62,4 @@ Drupal.behaviors.backToTop = {
   }
 }
 
-})(Drupal, once);
+})(jQuery, Drupal, once);
