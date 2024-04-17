9830008ea0c621bd4842d7b5f5b15bc593814ff1
Adam Fuchs <adam@electriccitizen.com>
2024-04-17 08:21:35 -0500

    Featured program cluster theming

--- web/themes/custom/mstate/components/03-composites/views/program-clusters/program-clusters.js
+++ web/themes/custom/mstate/components/03-composites/views/program-clusters/program-clusters.js
@@ -6,22 +6,32 @@
 	Drupal.behaviors.featuredClusters = {
 		attach: function (context, settings) {
 			$(once('clusters', '.program-clusters.featured-clusters', context)).each(function(){
+        console.log('clusters');
 				$(document).ready(function(){
-          console.log('slicking');
-					// $('.featured-clusters .view-content').slick({
-					// 	adaptiveHeight: false,
-					// 	autoplay: true,
-					// 	autoplaySpeed: 5000,
-          //   slidesToShow: 3,
-          //   slidesToScroll: 3,
-          //   settings: "slick",
-          //   responsive: [
-          //     {
-          //       breakpoint: 985,
-          //       settings: "unslick",
-          //     }
-          //   ]
-					// });
+					$('.program-clusters.featured-clusters .view-content').slick({
+						adaptiveHeight: false,
+						autoplay: true,
+						autoplaySpeed: 5000,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            settings: "slick",
+            responsive: [
+              {
+                breakpoint: 1440,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+              },
+              {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              }
+            ]
+					});
 				});//end doc ready
 			});//end once stats
 		}
