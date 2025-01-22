e2782903b52d150df5869265d466c9e4e14fc274
Wilbur Ince <wilbur@electriccitizen.com>
2025-01-22 15:19:00 -0600

    Adding onReady script for lazy images

--- tests/backstop/backstop_data/engine_scripts/puppet/onReady.js
+++ tests/backstop/backstop_data/engine_scripts/puppet/onReady.js
@@ -1,6 +1,18 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
+
+   // Ensure all images loaded before doing the screenshot
+  page.evaluate(async () => {
+    document.querySelectorAll('[loading="lazy"]').forEach((element) => {
+      element.loading = 'eager';
+    });
+
+    document.querySelectorAll('[decoding="async"]').forEach((element) => {
+      element.decoding = 'sync';
+    });
+  });
+
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
+
 };
