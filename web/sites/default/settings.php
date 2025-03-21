cf489ee2c2bfa2067a7fda0adcb2b3b8084b7072
Wilbur Ince <wilbur@electriccitizen.com>
2025-03-21 13:55:06 -0500

    Updated settings file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,29 +1,22 @@
 <?php
 
 /**
+ * @file
  * Load services definition file.
  */
+
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
+ * N.b. The settings.pantheon.php file makes some changes
  *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
  *      the site settings remain consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
-
-/**
- * Skipping permissions hardening will make scaffolding
- * work better, but will also raise a warning when you
- * install Drupal.
- *
- * https://www.drupal.org/project/drupal/issues/3091285
- */
-// $settings['skip_permissions_hardening'] = TRUE;
 
 /**
  * Place the config directory outside of the Drupal root.
@@ -33,7 +26,7 @@ $settings['config_sync_directory'] = "../config/sync";
 $settings['state_cache'] = TRUE;
 
 /**
- * Set up config splits
+ * Set up config splits.
  */
 if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
   $settings['file_temp_path'] = '/tmp';
@@ -41,42 +34,37 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
   switch ($_ENV['PANTHEON_ENVIRONMENT']) {
     case 'live':
       $config['config_split.config_split.live']['status'] = TRUE;
+      $config['system.logging']['error_level'] = 'hide';
       break;
 
     case 'test':
       $config['config_split.config_split.test']['status'] = TRUE;
+      $config['system.logging']['error_level'] = 'hide';
       break;
 
     default:
       $config['config_split.config_split.dev']['status'] = TRUE;
+      $config['system.logging']['error_level'] = 'verbose';
       break;
 
   }
-} else {
-    // LOCAL
-
+}
+else {
+  // LOCAL.
   $config['config_split.config_split.local']['status'] = TRUE;
+  $config['system.logging']['error_level'] = 'hide';
+}
 
-  // ddev settings we installed here!
-  $ddev_settings = __DIR__ . '/settings.ddev.php';
-  if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
-    require $ddev_settings;
-  } else {
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  include $ddev_settings;
+}
 
-  /**
-   * If it's not DDEV, we use docksal settings file
-   */
-  $docksal_settings = __DIR__ . "/settings.docksal.php";
-    if (is_readable($docksal_settings)) {
-        include $docksal_settings;
-    }
-  }
-
-  /**
-   * If there is a local settings file, then include it
-   */
-  $local_settings = __DIR__ . "/settings.local.php";
-  if (file_exists($local_settings)) {
-      include $local_settings;
-  }
+/**
+ * If there is a local settings file, then include it.
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
 }
