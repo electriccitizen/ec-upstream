39a692a257153242b46054105b6e5370d6bb06a5
Adam Fuchs <adam@electriccitizen.com>
2025-09-22 10:37:20 -0500

    Remove facets, fix header search block input, rename search results block

--- web/modules/custom/citizen_custom/src/Form/SiteSearchForm.php
+++ web/modules/custom/citizen_custom/src/Form/SiteSearchForm.php
@@ -65,7 +65,7 @@ class SiteSearchForm extends FormBase {
       '#label' => 'Search',
       '#title' => $this->t('Search'),
       '#title_display' => 'invisible',
-      '#name' => 'search_site_api_fulltext',
+      '#name' => 'site_search_api_fulltext',
       '#size' => 30,
       '#default_value' => '',
       '#placeholder' => 'Enter keyword(s)',
