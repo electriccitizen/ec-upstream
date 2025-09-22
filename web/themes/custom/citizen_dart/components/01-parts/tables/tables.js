865dcb9f1057f060b8402eea4053fd6eba955b8f
Adam Fuchs <adam@electriccitizen.com>
2025-09-22 10:18:09 -0500

    Temporarily create web search two column layout while trying to remove facets and web components search

--- web/themes/custom/citizen_dart/components/01-parts/tables/tables.js
+++ web/themes/custom/citizen_dart/components/01-parts/tables/tables.js
@@ -6,7 +6,7 @@
     attach: function (context, settings) {
       $(once('responsive_table', '.layout-container table:not(.ui-datepicker-calendar)', context)).each(function () {
         $(this).basictable({ breakpoint: 760, });
-
+        console.log('tabling');
         const theadElements = this.getElementsByTagName('thead');
         if (theadElements.length === 0) {
           this.classList.add('no-header');
